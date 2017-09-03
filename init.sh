f6c4f19c744885d521fb43b5e80cf4af6266f418
Sam Burba <contact@samburba.com>
2017-09-03 14:16:23 -0400

    Install enpass

--- init.sh
+++ init.sh
@@ -28,21 +28,14 @@ install_deb () {
 add_apt_source () {
     local friendly_name=$1
     local source_url=$2
-    local key_url_1=$3
-    local key_url_2=$4
+    local key_url=$3
 
-    local source_line="deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib"
-    if grep -Fxq "$source_line" /etc/apt/sources.list; then
-        echo "$source_url is already in the sources list"
-    else
-        sudo bash -c 'cat >> /etc/apt/sources.list' << EOF
-# Apt Repository for $friendly_name
-deb $source_url $(lsb_release -sc) contrib
-EOF
-    fi
+    local source_line="deb $source_url"
+    local source_file="/etc/apt/sources.list.d/$friendly_name.list"
 
-    wget -q "$key_url_1" -O- | sudo apt-key add -
-    wget -q "$key_url_2" -O- | sudo apt-key add -
+    sudo bash -c "echo $source_line > $source_file"
+
+    wget -q "$key_url" -O- | sudo apt-key add -
 }
 
 header () {
@@ -53,8 +46,12 @@ header () {
     echo
 }
 
+header "Adding required 3rd party repositories"
+add_apt_source enpass "http://repo.sinew.in/ stable main" https://dl.sinew.in/keys/enpass-linux.key
+
 header "Installing apps from the default repository"
-sudo apt install vim git stow i3 compton rofi lxappearance feh gdebi
+sudo apt update
+sudo apt install vim git stow i3 compton rofi lxappearance feh gdebi enpass
 
 header "Installing special snowflake apps not in the default repo"
 install_deb google-chrome-stable https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
